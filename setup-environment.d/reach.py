2b00d4ffff17a139c9e01261046b42e8d2728370
Stephano Cetola <scetola@reachtech.com>
2016-02-22 11:41:38 -0800

    initial checkin
    Signed-off-by: Stephano Cetola <scetola@reachtech.com>

---
+++ setup-environment.d/reach.py
@@ -0,0 +1,16 @@
+def __after_init_iet_yocto():
+    PLATFORM_ROOT_DIR = os.environ['PLATFORM_ROOT_DIR']
+    append_layers([ os.path.join(PLATFORM_ROOT_DIR, 'sources', p) for p in
+                    [
+                     'meta-fsl-arm',
+                     'meta-openembedded/meta-oe',
+                     'meta-openembedded/meta-ruby',
+                     'meta-reach',
+                     'meta-iet',
+                     'meta-qt5'
+                    ]])
+
+    # FSL EULA
+    eulas.accept['meta-fsl-arm/EULA'] = 'ACCEPT_FSL_EULA = "1"'
+
+run_after_init(__after_init_iet_yocto)
